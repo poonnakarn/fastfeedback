55da29194ce904fa8ba7616213af2cc7620bfdc5
Poonnakarn Panjasriprakarn <poonnakarn@gmail.com>
2022-02-28 16:50:53 +0700

    implement auth.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
+import { AuthProvider } from '../lib/auth'
+import { db } from '../lib/firebase'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  console.log(db)
+
+  return (
+    <AuthProvider>
+      <Component {...pageProps} />
+    </AuthProvider>
+  )
 }
 
 export default MyApp
