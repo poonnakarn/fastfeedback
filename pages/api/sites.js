7b61dce59044538d368a8f8658770f75395c6e92
Poonnakarn Panjasriprakarn <poonnakarn@gmail.com>
2022-03-02 10:58:34 +0700

    fix api bug

--- pages/api/sites.js
+++ pages/api/sites.js
@@ -1,4 +1,5 @@
 import db from '@/lib/firebase-admin'
+// import { getFirestore } from 'firebase-admin/firestore'
 // import { } from 'firebase-admin/firestore'
 // import { setDoc, doc, addDoc, collection } from 'firebase/firestore'
 
@@ -7,8 +8,7 @@ export default async (_, res) => {
   let sites = []
 
   snapshot.forEach((doc) => {
-    sites.push({ id: doc.id, ...doc })
-    console.log(doc.id, '=>', doc.data())
+    sites.push({ id: doc.id, ...doc.data() })
   })
 
   res.status(200).json(sites)
